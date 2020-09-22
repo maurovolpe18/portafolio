956df67d6517af4197eb2e8a507fd2d6d3b6eae7
maurovolpe18 <maurovolpe18@gmail.com>
2020-09-22 18:42:28 -0300

    contacto

--- src/components/pages/ContactoScreen.js
+++ src/components/pages/ContactoScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useForm } from "../../hooks/useForm";
-
+import mail from "../../img/mail.svg";
 export const ContactoScreen = () => {
   const [formValues, handleInputChange] = useForm({
     nombre: "",
@@ -14,38 +14,40 @@ export const ContactoScreen = () => {
     <>
       <div className="container">
         <h1 className="contacto__title">Contacto</h1>
-
-        <form className="contacto__formulario container">
-          <div className="contacto__info">
-            <input
-              type="text"
-              placeholder="Nombre"
-              name="nombre"
-              className="contacto__persona"
-              value={nombre}
+        <div className="contacto__container">
+          <img src={mail} alt={mail} className="contacto__imagen" />
+          <form className="contacto__formulario container">
+            <div className="contacto__info">
+              <input
+                type="text"
+                placeholder="Nombre"
+                name="nombre"
+                className="contacto__persona"
+                value={nombre}
+                onChange={handleInputChange}
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                name="email"
+                className="contacto__email"
+                value={email}
+                onChange={handleInputChange}
+              />
+            </div>
+            <textarea
+              className="contacto__mensaje"
+              name="texto"
+              value={texto}
               onChange={handleInputChange}
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              name="email"
-              className="contacto__email"
-              value={email}
-              onChange={handleInputChange}
-            />
-          </div>
-          <textarea
-            className="contacto__mensaje"
-            name="texto"
-            value={texto}
-            onChange={handleInputChange}
-            placeholder="Mensaje"
-          ></textarea>
+              placeholder="Mensaje"
+            ></textarea>
 
-          <button type="submit" className="boton contacto__boton-formulario">
-            Enviar
-          </button>
-        </form>
+            <button type="submit" className="boton contacto__boton-formulario">
+              Enviar
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
