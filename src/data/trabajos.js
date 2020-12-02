19f759f6b707cd2d931de1f677ee3278e0145f0b
maurovolpe18 <maurovolpe18@gmail.com>
2020-12-02 10:21:54 -0300

    arreglo de enlaces

--- src/data/trabajos.js
+++ src/data/trabajos.js
@@ -2,27 +2,29 @@ import netflix from "../img/netflix.png";
 import developer from "../img/developer.png";
 import pokedex from "../img/pokedex.png";
 
-export const trabajos = [{
-        id: 1,
-        nombre: "Netflix Clon",
-        img: netflix,
-        descripcion: "Un clon de netflix diseñado con la api 'The Movie Database'",
-        direccion: "https://competent-beaver-30cece.netlify.app",
-        git: "https://github.com/maurovolpe18/netflix-clon",
-    },
-    {
-        id: 2,
-        nombre: "Invoza",
-        img: developer,
-        direccion: "https://zealous-pike-c558d6.netlify.app/",
-        descripcion: "Proyecto de maquetación Frontend",
-        git: "https://github.com/EspacioDelDeveloper/FE-Team-B",
-    },
-    {
-        id: 3,
-        nombre: "Pokedex",
-        img: pokedex,
-        descripcion: "Un pokédex realizado con la api 'pokeapi'",
-        direccion: "https://eager-murdock-f9b247.netlify.app/",
-    },
+export const trabajos = [
+  {
+    id: 1,
+    nombre: "Netflix Clon",
+    img: netflix,
+    descripcion: "Un clon de netflix diseñado con la api 'The Movie Database'",
+    direccion: "https://competent-beaver-30cece.netlify.app",
+    git: "https://github.com/maurovolpe18/netflix-clon",
+  },
+  {
+    id: 2,
+    nombre: "Invoza",
+    img: developer,
+    direccion: "https://zealous-pike-c558d6.netlify.app/",
+    descripcion: "Proyecto de maquetación Frontend",
+    git: "https://github.com/maurovolpe18/invoza",
+  },
+  {
+    id: 3,
+    nombre: "Pokedex",
+    img: pokedex,
+    descripcion: "Un pokédex realizado con la api 'pokeapi'",
+    direccion: "https://eager-murdock-f9b247.netlify.app/",
+    git: "https://github.com/maurovolpe18/pokedex",
+  },
 ];
